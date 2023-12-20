7beb2ec680128b928380d703c01d7c80fc8c50cb
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2023-12-20 15:54:22 +0700

    menampilkan Todo item dan styling component TodoItem

--- app-todo/src/components/TodoItem.jsx
+++ app-todo/src/components/TodoItem.jsx
@@ -1,8 +1,31 @@
+import { RiDeleteBin2Fill, RiEditFill } from '@remixicon/react'
 import React from 'react'
 
-const TodoItem = () => {
+const TodoItem = ({id, task, isDone}) => {
+
   return (
-    <div>TodoItem</div>
+    <div className='flex justify-between items-center bg-violet-800
+     text-white py-3 px-4 mb-1 rounded-md'>
+        <span className='flex justify-center'> 
+            <input 
+                type="checkbox"
+                className=' mr-2'
+            />
+
+            {task}
+
+        </span>
+        <div className='flex items-center gap-x-4'>
+            <RiEditFill 
+                className='cursor-pointer'
+                size={20}
+            />
+            <RiDeleteBin2Fill 
+                className='cursor-pointer'
+                size={20}
+            />
+        </div>
+    </div>
   )
 }
 
--- app-todo/src/components/TodoList.jsx
+++ app-todo/src/components/TodoList.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import FormAddTodo from './FormAddTodo'
+import { useSelector } from 'react-redux'
+import TodoItem from './TodoItem';
 
 const TodoList = () => {
+    const {todos} = useSelector((state) => state.todo);
+    
+    // console.log(todos);
   return (
     <div className='container bg-gray-800 mt-20 p-8
     rounded-md'>
         <FormAddTodo></FormAddTodo>
+        <ul>
+            {
+                todos.map((todo) => (
+                    <TodoItem id={todo.id} task={todo.task}></TodoItem>
+                ))
+            }
+        </ul>
     </div>
   )
 }
